e88f92877ddb634328a1855f36677515503977bb
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-09-22 05:05:24 +0530

    cors allowed

--- api/server.js
+++ api/server.js
@@ -18,20 +18,7 @@ const allowedOrigins = [
   "http://localhost:5173/", // Add more origins as needed
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, origin);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
