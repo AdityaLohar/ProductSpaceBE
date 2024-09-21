67ce80a45f169f5fcf92076ea6d15750a2e53dab
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-09-22 05:10:04 +0530

    trial

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDb = require("../db/connectDb.js");
 const cors = require("cors");
 const { submitEnquiry } = require("../controllers/userController.js");
+const User = require('../models/userModel.js'); // Import your User model
 
 dotenv.config();
 
@@ -30,8 +31,44 @@ app.get("/api/submit-enquiry", (req, res) => {
   res.status(200).json({ msg: "Enquiry Submit" });
 });
 
-app.post("/api/submit-enquiry", submitEnquiry);
-app.post("/api/formdata", submitEnquiry);
+app.post("/api/submit-enquiry", async (req, res) => {
+    try {
+        console.log("Entered in function");
+        const { name, email, phone } = req.body; // Get data from request body
+        const user = await User.findOne({ email }); // Check for existing user
+        console.log("User searched");
+        
+        if (user) {
+            return res.status(401).json({ error: "User already exists" });
+        }
+        
+        const newUser = new User({
+            name,
+            email,
+            phone,
+        });
+        
+        await newUser.save();
+        console.log("User saved");
+
+        if (newUser) {
+            res.status(201).json({
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                phone: newUser.phone,
+            });
+        } else {
+            res.status(401).json({ error: "Invalid user data" });
+        }
+
+    } catch (err) {
+        res.status(501).json({ error: err.message });
+        console.log("Error in submit enquiry: ", err.message);
+    }
+});
+
+
 app.options("/api/submit-enquiry", (req, res) => {
   res.sendStatus(200);
 });
